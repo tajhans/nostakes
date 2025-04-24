fa87faa79d15d5d594516768984ff0389fdb27bc
Taj Hans <account@tajhans.com>
2025-04-24 12:51:28 -0500

    Update auth-client.ts

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
+console.log("VITE_SERVER_URL:", import.meta.env.VITE_SERVER_URL);
+
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_SERVER_URL,
 	plugins: [usernameClient()],
